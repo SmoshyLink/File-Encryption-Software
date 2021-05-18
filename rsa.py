919fede7b405af0a438a89075c6cca1b73ecdde2
Nizar Masri <nizarmasri37@gmail.com>
2021-05-18 23:17:43 +0300

    Algorrithm for RSA

--- rsa.py
+++ rsa.py
@@ -1,16 +1,86 @@
 #take message string
 #encrypt and print
 #decrypt and print
-
+import os
 import random
 
-#HELPER FUNCTION: check if numbers are coprime
+#Open file function ---- takes filename string 
+def open_file(filename):
+	file = open(filename, "rb")
+	byte = file.read(8)
+	byte2hex=byte
+	while byte:
+		byte = file.read(8)
+		byte2hex+=byte
+	file.close()
+	plaintext=byte2hex.hex().upper()
+
+    #returns a hex string of file data (converted from byte to hex)
+	return plaintext 
+
+# Hexadecimal to binary conversion
+def hex2bin(s):
+	mp = {'0' : "0000",
+		'1' : "0001",
+		'2' : "0010",
+		'3' : "0011",
+		'4' : "0100",
+		'5' : "0101",
+		'6' : "0110",
+		'7' : "0111",
+		'8' : "1000",
+		'9' : "1001",
+		'A' : "1010",
+		'B' : "1011",
+		'C' : "1100",
+		'D' : "1101",
+		'E' : "1110",
+		'F' : "1111",
+		' ' : "100000"
+  }
+	bin = ""
+	for i in range(len(s)):
+		bin = bin + mp[s[i]]
+	return bin
+	
+# Binary to hexadecimal conversion
+def bin2hex(s):
+	mp = {"0000" : '0',
+		"0001" : '1',
+		"0010" : '2',
+		"0011" : '3',
+		"0100" : '4',
+		"0101" : '5',
+		"0110" : '6',
+		"0111" : '7',
+		"1000" : '8',
+		"1001" : '9',
+		"1010" : 'A',
+		"1011" : 'B',
+		"1100" : 'C',
+		"1101" : 'D',
+		"1110" : 'E',
+		"1111" : 'F',
+		"100000" : ' '
+  }
+	hex = ""
+	for i in range(0,len(s),4):
+		ch = ""
+		ch = ch + s[i]
+		ch = ch + s[i + 1]
+		ch = ch + s[i + 2]
+		ch = ch + s[i + 3]
+		hex = hex + mp[ch]
+		
+	return hex
+
+#HELPER FUNCTION: check if numbers are coprime --- int argument
 def gcd(a,b):
     while b != 0:
         a, b = b, a % b
     return a
 
-#HELPER FUNCTION: check if integers are primes
+#HELPER FUNCTION: check if integers are primes --- int argument
 def isPrime(numb):
     if numb == 2:
         return True
@@ -24,7 +94,7 @@ def isPrime(numb):
 #list of prime numbers global variable
 primes = [i for i in range(0,100) if isPrime(i)]
 
-#HELPER FUNCTION: finding d
+#HELPER FUNCTION: finding d -- int arg
 def mod_inverse(a,b):
     x1=0
     x2=1
@@ -51,7 +121,6 @@ def mod_inverse(a,b):
     return x2
 
 
-
 #Generate a pair of public and private keys
 def create_keys():
     p = random.choice(primes)
@@ -76,18 +145,27 @@ def create_keys():
     #return public and private key tuples
     return ((e,n),(d,n))
 
-#Function to encrypt plaintext
-def encrypt(publikKey, plainText):
-    
-    e, n = publikKey
+
+#Function to encrypt plaintext; --- tuple(publicKey) and hex string (plainText)
+def encrypt(publicKey, plainText):
+    e, n = publicKey
+
+    #apply RSA ecnryption equation: (message^e)mod n
     cipherText = [(ord(char) ** e) % n for char in plainText]
+    
+    #return list of integers
     return cipherText
 
-#function to decrypt ciphertext
+
+#function to decrypt ciphertext--- tuple(privateKey) and hex string (cipherText)
 def decrypt(privateKey, cipherText):
     
     d, n = privateKey
+
+    #apply RSA decryption equation: (message^d)mod n
     plainText = [chr((char ** d) % n) for char in cipherText]
+
+    #return hex string
     return ''.join(plainText)
 
 
@@ -95,8 +173,30 @@ def decrypt(privateKey, cipherText):
 print("You have chosen RSA encryption\nGenerating keys......")
 publicKey, privateKey = create_keys()
 print("Public key: ",publicKey, "\nPrivate key: ", privateKey)
-plainText=input("Type a message to encrypt: ")
-cipherText=encrypt(publicKey, plainText)
-print("This is your cipher text: ", ''.join(map(str, cipherText)))
-print("Now decrypting......")
-print("This is your decrypted text: ", decrypt(privateKey, cipherText))
+
+spacer=".\n"*10
+
+file_location = "user input from GUI"
+file_extension = os.path.splitext(file_location)
+
+plain_text = open_file(file_location)
+print("PLAIN TEXT: ","\n",plain_text)
+
+cipher_text=encrypt(publicKey,plain_text)
+cipher_text2=''.join([chr(char) for char in cipher_text])
+
+'''with open("2.txt", "wb") as f:
+    f.write(bytes.fromhex(cipher_text2))'''
+
+print(spacer,"ENCRYPTION: ","\n",cipher_text)
+
+decrypted_text=decrypt(privateKey,cipher_text)
+print(spacer,"DECRYPTION: ","\n",decrypted_text)
+
+print(spacer,"CHECKING THE TEXTS ARE EQUAL:\n",plain_text==decrypted_text)
+
+dec_file = "dec" + file_extension
+
+# CREATE DECRYPTED FILE =============================
+with open(dec_file, "wb") as f:
+    f.write(bytes.fromhex(decrypted_text)) #conert hex string to bytes and write bytes to file
